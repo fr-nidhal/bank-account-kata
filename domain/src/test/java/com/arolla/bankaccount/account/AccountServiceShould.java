a4d973ed4e3417c61626ac61121c1268487ba058
dr.nido <fr.nidhal@gmail.com>
2019-05-27 01:58:18 +0200

    migration junit 5

--- domain/src/test/java/com/arolla/bankaccount/account/AccountServiceShould.java
+++ domain/src/test/java/com/arolla/bankaccount/account/AccountServiceShould.java
@@ -2,11 +2,10 @@ package com.arolla.bankaccount.account;
 
 import com.arolla.bankaccount.DateHandler;
 import com.arolla.bankaccount.statement.StatementsPrinter;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
@@ -17,10 +16,12 @@ import static java.util.Arrays.asList;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@DisplayName("The Account Service Should")
 public class AccountServiceShould {
     private static final String TODAY = "04/04/2019";
-    private AccountService accountService ;
+    private AccountService accountService;
 
     @Mock
     AccountID accountID;
@@ -31,41 +32,39 @@ public class AccountServiceShould {
     @Mock
     DateHandler dateHandler;
 
-    @Before
-    public void initialize(){
-        accountService =  new AccountService(accountRepository, statementsPrinter,dateHandler);
+    @BeforeEach
+    void initialize() {
+        accountService = new AccountService(accountRepository, statementsPrinter, dateHandler);
     }
 
 
-
     @Test
-    public void store_a_deposit_operation_into_my_account(){
+    void store_a_deposit_operation_into_my_account() {
 
         given(dateHandler.today()).willReturn(TODAY);
         //when
-        accountService.deposit(accountID,amountOf(300));
+        accountService.deposit(accountID, amountOf(300));
         //then
-        verify(accountRepository).addOperation(accountID,new Operation(TODAY,amountOf(300), DEPOSIT));
+        verify(accountRepository).addOperation(accountID, new Operation(TODAY, amountOf(300), DEPOSIT));
 
     }
 
     @Test
-    public void store_a_withdraw_operation_into_my_account(){
+    void store_a_withdraw_operation_into_my_account() {
 
         given(dateHandler.today()).willReturn(TODAY);
         //when
-        accountService.withdraw(accountID,amountOf(300));
+        accountService.withdraw(accountID, amountOf(300));
         //then
-        verify(accountRepository).addOperation(accountID,new Operation(TODAY,amountOf(300), WITHDRAW));
+        verify(accountRepository).addOperation(accountID, new Operation(TODAY, amountOf(300), WITHDRAW));
 
     }
 
     @Test
-    public void print_all_my_account_statements(){
+    void print_all_my_account_statements() {
 
-        List<Operation> operations = asList(new Operation(TODAY,amountOf(300), DEPOSIT));
+        List<Operation> operations = asList(new Operation(TODAY, amountOf(300), DEPOSIT));
         given(accountRepository.allOperations(accountID)).willReturn(operations);
-
 
 
         accountService.printStatements(accountID);
